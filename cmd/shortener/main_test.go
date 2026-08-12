7088d7f6504902b4510f8d7fefd520e6b9acb97b
Piktet <azopkov.d@gmail.com>
2026-08-12 14:28:57 +0300

    iter19_v7

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_run(t *testing.T) {
+func Test_runSrv(t *testing.T) {
 
 	ctx, fnTimeoutCancel := context.WithTimeoutCause(context.Background(), time.Second*2, errors.New("stop timeout test"))
 	defer fnTimeoutCancel()
@@ -25,7 +25,7 @@ func Test_run(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			err := run(context.WithCancelCause(ctx))
+			err := runSrv(context.WithCancelCause(ctx))
 			if test.wantErr {
 				assert.Error(t, err)
 				return
