ef19a8de83c5380c76ead3e30b6d0ebd3ea40921
Piktet <azopkov.d@gmail.com>
2026-08-12 14:31:43 +0300

    iter19_v8

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -12,31 +12,6 @@ import (
 
 func Test_runSrv(t *testing.T) {
 
-	ctx, fnTimeoutCancel := context.WithTimeoutCause(context.Background(), time.Second*2, errors.New("stop timeout test"))
-	defer fnTimeoutCancel()
-	tests := []struct {
-		name    string // description of this test case
-		wantErr bool
-	}{
-		{
-			name:    "positive",
-			wantErr: false,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			err := runSrv(context.WithCancelCause(ctx))
-			if test.wantErr {
-				assert.Error(t, err)
-				return
-			}
-			assert.NoError(t, err)
-		})
-	}
-}
-
-func Test_run(t *testing.T) {
-
 	ctx, fnCancel := context.WithTimeoutCause(context.Background(), time.Second*2, errors.New("test stop with timeout 2 seconds"))
 	defer fnCancel()
 	tests := []struct {
@@ -53,7 +28,7 @@ func Test_run(t *testing.T) {
 			listner := &http.Server{
 				Addr: ":8000",
 			}
-			run(ctx, listner)
+			runSrv(ctx, listner)
 		})
 	}
 }
